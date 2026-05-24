dc68e6cd7f13f78e4538fd5238a3c01ebc8ad651
Taanviir <66136914+Taanviir@users.noreply.github.com>
2026-05-24 15:46:32 +0400

    refactor: replace tools import with server import in main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 	"github.com/tanas/mcp42/intra"
-	"github.com/tanas/mcp42/tools"
+	"github.com/tanas/mcp42/server"
 )
 
 func main() {
